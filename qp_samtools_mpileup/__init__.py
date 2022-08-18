2bec4633df525554ca878e9eb285dfac9aa9045d
Grant Cheng <chengxgrant@gmail.com>
2022-08-18 12:55:22 -0700

    Updated __init__.py file

--- qp_samtools_mpileup/__init__.py
+++ qp_samtools_mpileup/__init__.py
@@ -21,23 +21,21 @@ plugin = QiitaPlugin(**plugin_details)
 # Define the command
 ref = get_ref()
 ref_without_extension = splitext(ref)[0]
-dbs_defaults = ', '.join([f'"{x}"' for x in dbs_without_extension])
+# dbs_defaults = ', '.join([f'"{x}"' for x in dbs_without_extension])
 req_params = {'input': ('artifact', ['per_sample_FASTQ'])}
 opt_params = {
-    'reference': [
-        f'choice:["None", {dbs_defaults}]', dbs_without_extension[0]],
-    'threads': ['integer', f'{THREADS}']}
+    'reference': ['string', f'{ref_without_extension}']}
 
 outputs = {'Filtered files': 'per_sample_FASTQ'}
 default_params = {
-    'auto-detect adapters only filtering [not recommended]': {
-        'reference': "None", 'threads': THREADS}}
-for db in dbs_without_extension:
-    name = f'auto-detect adapters and {db} + phix filtering'
-    default_params[name] = {'reference': db, 'threads': THREADS}
+    'default params': {
+        'reference': "covid-ref"}}
+# for db in dbs_without_extension:
+#     name = f'auto-detect adapters and {db} + phix filtering'
+#     default_params[name] = {'reference': db, 'threads': THREADS}
 
-samtools_pileup_cmd = QiitaCommand(
-    'Samtools pileup', "Pileup using samtools",
-    samtools_pileup, req_params, opt_params, outputs, default_params)
+samtools_mpileup_cmd = QiitaCommand(
+    'Samtools mpileup', "Mpileup using samtools",
+    samtools_mpileup, req_params, opt_params, outputs, default_params)
 
-plugin.register_command(samtools_pileup_cmd)
+plugin.register_command(samtools_mpileup_cmd)
